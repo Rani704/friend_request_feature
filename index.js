e2df1e4c19314fddc707255efe75197b235740d6
Rani <rani7042003@gmail.com>
2024-01-10 11:59:53 +0530

    added my project

---
+++ index.js
@@ -0,0 +1,17 @@
+var istatus=document.querySelector("h5");
+var add=document.querySelector('#add');
+
+var check=0;
+add.addEventListener("click",function(){
+    if(check==0){ istatus.innerHTML="Friends"
+    istatus.style.color='green'
+    add.innerHTML="Remove friend";
+check=1;}
+else
+    { istatus.innerHTML="Stranger"
+    istatus.style.color='red'
+    add.innerHTML="Add friend"
+    check=0;
+}
+   
+});
